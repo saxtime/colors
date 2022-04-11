548c5a362fc5be680ca4e2a30b8cc9f0617961ea
saxtime <saxtime@yandex.ru>
2022-04-11 20:21:39 +0300

    Initial Commit

---
+++ colors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  colors
+//
+//  Created by Андрей Василевский on 4/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ colors/colorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  colorsApp.swift
+//  colors
+//
+//  Created by Андрей Василевский on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct colorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
